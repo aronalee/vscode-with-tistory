6a4b4f1d557350d5d556c2cba6e4b342911930d7
dev-green-flamingo <devgflamingo@gmail.com>
2021-08-20 18:40:09 +0900

    Parsing Port

--- src/Client.ts
+++ src/Client.ts
@@ -6,22 +6,23 @@ const client = http.createServer(
     (req: http.IncomingMessage, res: http.ServerResponse) => {
         const { url } = req;
         const params = new URL(`http://localhost${url}`).searchParams;
-        const [code, error] = [params.get("code"), params.get("error")];
+        const [code, error,error_description] = [
+            params.get("code"), params.get("error"),params.get("error_description")];
         if (code !== null) {
             createAccessToken(code);
             res.writeHead(200);
             res.write(`close WebPage`);
         } else {
             res.writeHead(200);
-            res.write(`Tistory Error: ${error}`);
+            res.write(`Tistory Error: ${error} => ${error_description}`);
         }
         res.end();
     }
 );
 
-export const runClient = (): void => {
+export const runClient = (port: number): void => {
     if (!client.listening) {
-        client.listen(5500, () => {
+        client.listen(port, () => {
             console.log("Start Client");
         });
     }
--- src/extension.ts
+++ src/extension.ts
@@ -7,22 +7,36 @@ import {
 } from "./apis";
 import { runClient, stopClient } from "./Client";
 import { KIND_OF_CERTIFICATE, PROPERTIES } from "./Enum";
+import { ConfigType } from "./interface";
 
 export function activate(context: vscode.ExtensionContext) {
     const loginTistory = vscode.commands.registerCommand(
         "vscode-with-tistory.login",
         () => {
-            const redirectURI = getConfigProperty(PROPERTIES.RedirectURI);
-            const clientID = getConfigProperty(PROPERTIES.ClientID);
-            const accessToken = getConfigProperty(PROPERTIES.Token);
-            runClient();
-            const result=certifyTistory(accessToken, clientID, redirectURI);
-            if(result===KIND_OF_CERTIFICATE.OpenBrowser){
+            const redirectURI: ConfigType = getConfigProperty(
+                PROPERTIES.RedirectURI
+            );
+            const clientID: ConfigType = getConfigProperty(PROPERTIES.ClientID);
+            const accessToken: ConfigType = getConfigProperty(PROPERTIES.Token);
+            let port = 5500;
+            if (redirectURI) {
+                const isPort = redirectURI?.match(/(?<=\:)\d{1,5}/);
+                if (isPort) {
+                    port = parseInt(isPort[0]);
+                }
+            }
+            runClient(port);
+            const result = certifyTistory(accessToken, clientID, redirectURI);
+            if (result === KIND_OF_CERTIFICATE.OpenBrowser) {
                 vscode.window.showInformationMessage("브라우저를 확인해주세요");
-            }else if(result===KIND_OF_CERTIFICATE.HasToken){
+            } else if (result === KIND_OF_CERTIFICATE.HasToken) {
                 vscode.window.showErrorMessage("토큰이 존재합니다.");
-            }else if(result===KIND_OF_CERTIFICATE.HasNotProperty){
-                vscode.window.showErrorMessage("Client.OAuth2 속성을 확인해주세요")
+                stopClient();
+            } else if (result === KIND_OF_CERTIFICATE.HasNotProperty) {
+                vscode.window.showErrorMessage(
+                    "Client.OAuth2 속성을 확인해주세요"
+                );
+                stopClient();
             }
         }
     );
@@ -40,9 +54,7 @@ export function activate(context: vscode.ExtensionContext) {
     );
     const pushAll = vscode.commands.registerCommand(
         "vscode-with-tistory.push-all",
-        () => {
-            runClient();
-        }
+        () => {}
     );
 
     context.subscriptions.push(loginTistory, getBlog);
